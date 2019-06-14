62b99f859193eb233012038bb3a34b494aace1cb
StarrKiss <gregorystarr00@gmail.com>
2019-06-14 12:18:16 -0500

    Added jumping and gliding
    PUSH

---
+++ Platformer/Assets/gravity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gravity : MonoBehaviour
+{
+    public float gravityAmount = 9.81f;
+     Rigidbody rb;
+
+
+  public void Start(){
+    rb = GetComponent<Rigidbody>();
+  }
+
+   public void FixedUpdate() {
+
+       rb.AddForce(transform.up * -gravityAmount);
+
+   }
+}
---
+++ Platformer/Assets/playerControl.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour
+{
+
+     public float glideSpeed;
+     private Rigidbody rb;
+     private float distanceToGround;
+     public float jumpSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per second
+    void FixedUpdate()
+    {
+       //GroundCheck
+       RaycastHit hit = new RaycastHit();
+              if (Physics.Raycast (transform.position, -Vector3.up, out hit)) {
+                   distanceToGround = hit.distance - 0.5f;
+
+
+              }
+
+
+
+
+              //gliding
+       if(Input.GetButton("Jump") && distanceToGround != 0){
+          Vector3 vel = rb.velocity;
+           vel.y = -glideSpeed;
+
+         rb.velocity = vel;
+
+       }
+       else{}
+        //jumping
+        if(Input.GetButtonDown("Jump") && distanceToGround == 0){
+          rb.AddForce(transform.up * jumpSpeed);
+        }
+
+
+    }
+}
